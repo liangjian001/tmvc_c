85d32704ee5817f456d56517d1d533396757d644
liangjian001 <294566692@qq.com>
2017-05-10 11:33:18 +0800

    update class

--- src/test/java/method/TestStatic.java
+++ src/test/java/method/TestStatic.java
@@ -10,15 +10,15 @@ public class TestStatic extends FinalClass{
 		TestStatic t1 = new TestStatic();
 		TestStatic.str1 ="002B";
 		System.out.println(t1.getCode());
-		System.out.println(t1.getStr1());
+		System.out.println(TestStatic.getStr1());
 		
 		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>");
 		
 		TestStatic t2 = new TestStatic();
 		System.out.println(t2.getCode());
-		System.out.println(t2.getStr1());
+		System.out.println(TestStatic.getStr1());
 		
-		System.out.println(t2.test1());
+		System.out.println(TestStatic.test1());
 		
 	}
 
